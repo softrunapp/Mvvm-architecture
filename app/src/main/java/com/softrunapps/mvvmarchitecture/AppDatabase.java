9e8b25db78864eb3e37a7042e3aa091cff819b28
meisam <softrunapps@gmail.com>
2019-10-05 16:01:31 +0330

    add callback for database add 4 column data for onCreate callback

--- app/src/main/java/com/softrunapps/mvvmarchitecture/AppDatabase.java
+++ app/src/main/java/com/softrunapps/mvvmarchitecture/AppDatabase.java
@@ -1,10 +1,13 @@
 package com.softrunapps.mvvmarchitecture;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = User.class, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
@@ -17,8 +20,34 @@ public abstract class AppDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     AppDatabase.class, "app_db")
                     .fallbackToDestructiveMigration()
+                    .addCallback(callback)
                     .build();
         }
         return instance;
+    }
+
+    private static Callback callback = new Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulatedDbAsyncTask(instance.userDao()).execute();
+        }
+    };
+
+    private static class PopulatedDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private UserDao userDao;
+
+        public PopulatedDbAsyncTask(UserDao userDao) {
+            this.userDao = userDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            userDao.insert(new User("john", "johon@gmail.com"));
+            userDao.insert(new User("sara", "sara@gmail.com"));
+            userDao.insert(new User("cris", "rcis@gmail.com"));
+            userDao.insert(new User("rose", "rose@gmail.com"));
+            return null;
+        }
     }
 }
