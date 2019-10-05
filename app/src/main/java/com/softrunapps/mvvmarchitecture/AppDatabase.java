9215a224110dc3fa9d4734a41e056fa18339df1b
meisam <softrunapps@gmail.com>
2019-10-05 13:29:57 +0330

    add AppDatabase

---
+++ app/src/main/java/com/softrunapps/mvvmarchitecture/AppDatabase.java
@@ -0,0 +1,24 @@
+package com.softrunapps.mvvmarchitecture;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = User.class, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase instance;
+
+    public abstract UserDao userDao();
+
+    public static AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    AppDatabase.class, "app_db")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+}
