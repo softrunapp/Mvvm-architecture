4621e5982b87e9be03d7c055703e987585afb97a
meisam <softrunapps@gmail.com>
2019-10-05 13:18:30 +0330

    Add user entity

--- app/src/main/java/com/softrunapps/mvvmarchitecture/User.java
+++ app/src/main/java/com/softrunapps/mvvmarchitecture/User.java
@@ -1,4 +1,30 @@
 package com.softrunapps.mvvmarchitecture;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
 public class User {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String usernsme;
+    private String email;
+
+    public User(String usernsme, String email) {
+        this.usernsme = usernsme;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsernsme() {
+        return usernsme;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
+
