f4c4bd23f6c87e7a3b806af9fe78c9250adb6744
meisam <softrunapps@gmail.com>
2019-10-05 13:39:22 +0330

    add synchronized to getInstance()

--- app/src/main/java/com/softrunapps/mvvmarchitecture/AppDatabase.java
+++ app/src/main/java/com/softrunapps/mvvmarchitecture/AppDatabase.java
@@ -12,7 +12,7 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public abstract UserDao userDao();
 
-    public static AppDatabase getInstance(Context context) {
+    public static synchronized AppDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     AppDatabase.class, "app_db")
