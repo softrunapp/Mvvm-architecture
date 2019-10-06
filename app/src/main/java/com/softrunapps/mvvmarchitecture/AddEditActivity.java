2370efec9d803c631589a7d996689be53c23c537
meisam <softrunapps@gmail.com>
2019-10-06 09:56:35 +0330

    add AddEditActivity

---
+++ app/src/main/java/com/softrunapps/mvvmarchitecture/AddEditActivity.java
@@ -0,0 +1,14 @@
+package com.softrunapps.mvvmarchitecture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit);
+    }
+}
