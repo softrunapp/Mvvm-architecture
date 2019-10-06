d36e93b20920caed71fd3a0243c87f3b8d24c4c6
meisam <softrunapps@gmail.com>
2019-10-06 11:20:06 +0330

    add onActivityResult() for save or update user

--- app/src/main/java/com/softrunapps/mvvmarchitecture/MainActivity.java
+++ app/src/main/java/com/softrunapps/mvvmarchitecture/MainActivity.java
@@ -1,9 +1,12 @@
 package com.softrunapps.mvvmarchitecture;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -11,9 +14,14 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int ADD_USER_REQUEST = 1;
+    public static final int EDIT_USER_REQUEST = 2;
+
     UserViewModel userViewModel;
     RecyclerView recyclerView;
     UserAdapter userAdapter;
@@ -22,9 +30,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFloatingButton();
         initAdapter();
         initRecyclerView();
         initSwipe();
+        initViewModel();
+
+    }
+
+    private void initViewModel() {
         userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
         userViewModel.getAll().observe(this, new Observer<List<User>>() {
             @Override
@@ -32,7 +46,17 @@ public class MainActivity extends AppCompatActivity {
                 userAdapter.submitList(users);
             }
         });
+    }
 
+    private void initFloatingButton() {
+        FloatingActionButton floatingActionButton = findViewById(R.id.floating_button_add);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddEditActivity.class);
+                startActivityForResult(intent, ADD_USER_REQUEST);
+            }
+        });
     }
 
     private void initSwipe() {
@@ -63,8 +87,44 @@ public class MainActivity extends AppCompatActivity {
         userAdapter.setOnItemClickListener(new UserAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(User user) {
-                Toast.makeText(MainActivity.this, user.getEmail(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, AddEditActivity.class);
+                intent.putExtra(AddEditActivity.USER_ID, user.getId());
+                intent.putExtra(AddEditActivity.USERNAME, user.getUsernsme());
+                intent.putExtra(AddEditActivity.EMAIL, user.getEmail());
+                startActivityForResult(intent, EDIT_USER_REQUEST);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ADD_USER_REQUEST && resultCode == RESULT_OK) {
+            String username = data.getStringExtra(AddEditActivity.USERNAME);
+            String email = data.getStringExtra(AddEditActivity.EMAIL);
+
+            User user = new User(username, email);
+            userViewModel.insert(user);
+
+            Toast.makeText(this, "User saved", Toast.LENGTH_SHORT).show();
+        } else if (requestCode == EDIT_USER_REQUEST && resultCode == RESULT_OK) {
+            int id = data.getIntExtra(AddEditActivity.USER_ID, -1);
+            if (id == -1) {
+                Toast.makeText(this, "User can't be updated", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            String username = data.getStringExtra(AddEditActivity.USERNAME);
+            String email = data.getStringExtra(AddEditActivity.EMAIL);
+            
+            User user = new User(username, email);
+            user.setId(id);
+            
+            userViewModel.update(user);
+
+            Toast.makeText(this, "User Updated", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "User not saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
